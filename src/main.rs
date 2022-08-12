d47002cba5d2aa9e2d5ac1cb3824af74e5c21697
Awab <awab.qureshi@gmail.com>
2022-08-12 14:04:37 +0500

    Removed coords struct + made board 1D array + redid print for board

--- src/main.rs
+++ src/main.rs
@@ -4,54 +4,59 @@ fn main() {
     let mut board = Board::new();
     print!("{board}");
 
-    let coords = Coords::new(1, 2).unwrap();
-    board.substitute(&coords, 'x');
-    print!("{board}");
+    // let coords = Coords::new(1, 2).unwrap();
+    // board.substitute(&coords, 'x');
+    // print!("{board}");
 }
 
-struct Coords(u8, u8);
+// struct Coords(u8, u8);
 
-impl Coords {
-    fn new(x: u8, y: u8) -> Result<Coords, String> {
-        if x < 3 && y < 3 {
-            Ok(Coords(x, y))
-        } else {
-            Err("Coords out of bounds".to_string())
-        }
-    }
-}
+// impl Coords {
+//     fn new(x: u8, y: u8) -> Result<Coords, String> {
+//         if x < 3 && y < 3 {
+//             Ok(Coords(x, y))
+//         } else {
+//             Err("Coords out of bounds".to_string())
+//         }
+//     }
+// }
 
 struct Board {
-    vals: [[char; 3]; 3],
+    vals: [char; 9],
 }
 
 impl Board {
     fn new() -> Board {
-        Board {
-            vals: [[' '; 3]; 3],
-        }
+        Board { vals: [' '; 9] }
     }
 
-    fn substitute(&mut self, coords: &Coords, character: char) -> &mut Self {
-        let (x, y) = (coords.0 as usize, coords.1 as usize);
-        self.vals[y][x] = character;
-        self
-    }
+    // fn substitute(&mut self, character: char) -> &mut Self {
+    //     let (x, y) = (coords.0 as usize, coords.1 as usize);
+    //     self.vals[y][x] = character;
+    //     self
+    // }
 }
 
 impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for row in self.vals {
-            for cell in row {
-                match write!(f, "|{cell}") {
+        for i in 0..self.vals.len() {
+            let cell = self.vals[i];
+            if i % 3 == 0 && i != 0 {
+                match writeln!(f, "|") {
+                    // match writeln!(f, "|") {
                     Ok(()) => {}
                     Err(_) => panic!("Cannot print to console"),
                 };
             }
-            match writeln!(f, "|") {
+            match write!(f, "|{cell}") {
+                // match writeln!(f, "|") {
                 Ok(()) => {}
                 Err(_) => panic!("Cannot print to console"),
             };
+        }
+        match write!(f, "|") {
+            Ok(()) => {}
+            Err(_) => panic!("Cannot print to console"),
         }
         Ok(())
     }
